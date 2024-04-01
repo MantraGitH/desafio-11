a09d02b51761edbb237b4f53015951c0cf756bfe
MantraGitH <camilo.arias56@hotmail.com>
2024-04-01 01:33:01 -0300

    comited

--- src/docs/info.js
+++ src/docs/info.js
@@ -2,9 +2,9 @@ export const info = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API E-COMMERCE",
+      title: "Desafio 11",
       version: "1.0.0",
-      description: "API de comercio online",
+      description: "Aplicacion de comercio online",
     },
     servers: [
       {
